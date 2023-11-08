3b081ea0bec2eb82005ef79eb6aab8c11415577b
Hyunwoo Cho <barefoot1423@gmail.com>
2023-11-07 21:42:46 -0500

    commit lecture 97

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/expenses.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home:,
-  ),)
+  runApp(
+    MaterialApp(
+      home: Expenses(),
+    ),
+  );
 }
