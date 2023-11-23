e1bc7cd3d9fec581393684ca3a471c433871f046
Hyunwoo Cho <barefoot1423@gmail.com>
2023-11-22 22:21:14 -0500

    commit lecture 119

--- lib/widgets/New_expense.dart
+++ lib/widgets/New_expense.dart
@@ -61,12 +61,16 @@ class _NewExpenseState extends State<NewExpense> {
       return;
     }
 
-    widget.onAddExpense(Expense(
-      title: _titleController.text,
-      amount: enteredAmount,
-      date: _selectedDate!,
-      category: _selectedCategory,
-    ));
+    widget.onAddExpense(
+      Expense(
+        title: _titleController.text,
+        amount: enteredAmount,
+        date: _selectedDate!,
+        category: _selectedCategory,
+      ),
+    );
+
+    Navigator.pop(context);
   }
 
   @override
@@ -79,7 +83,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16, 41, 16, 16),
       child: Column(
         children: [
           TextField(
